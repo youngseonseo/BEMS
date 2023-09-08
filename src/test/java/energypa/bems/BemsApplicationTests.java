0c3062cff9021601f618ba4b76f86af6d735a5be
youngseonseo <sue000731@naver.com>
2023-09-08 15:06:57 +0900

    [add] 기본 파일 설정 추가

---
+++ src/test/java/energypa/bems/BemsApplicationTests.java
@@ -0,0 +1,13 @@
+package energypa.bems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
