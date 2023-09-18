3e7e813b88bf16a861bb200fe1000ddb8faf830a
youngseonseo <sue000731@naver.com>
2023-09-18 11:17:12 +0900

    [add] swagger3 의존성 추가

---
+++ src/main/java/energypa/bems/swagger/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package energypa.bems.swagger;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("BEMS의 REST API")
+                .description("Springdoc을 사용한 BEMS의 REST API")
+                .version("1.0.0");
+    }
+}
