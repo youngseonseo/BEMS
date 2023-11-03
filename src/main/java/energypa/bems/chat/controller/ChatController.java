c23991ec70df67e3e48d2040d8d1dd00abea3b02
hyeonji lee <hyunji3017@naver.com>
2023-11-03 14:53:33 +0900

    [docs] 채팅방 생성, 조회 API 등록

--- src/main/java/energypa/bems/chat/controller/ChatController.java
+++ src/main/java/energypa/bems/chat/controller/ChatController.java
@@ -4,6 +4,13 @@ import energypa.bems.chat.entity.ChatMessage;
 import energypa.bems.chat.entity.ChatRoom;
 import energypa.bems.chat.entity.MessageType;
 import energypa.bems.chat.service.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -11,22 +18,67 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "chat", description = "채팅 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/chat")
+@RequestMapping("/api/chat")
 public class ChatController {
 
     private final ChatService chatService;
     private final SimpMessageSendingOperations messagingTemplate;
 
+    @Operation(summary = "채팅방 생성 요청", description = "유저가 입력한 이름을 가진 채팅방 생성")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "방 생성 성공",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChatRoom.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "방 생성 실패",
+                    content = @Content(mediaType = "application/json")
+            )
+    })
     @PostMapping
-    public ChatRoom createRoom(@RequestParam String roomName) {
+    public ChatRoom createRoom(@RequestParam("roomName") String roomName) {
         return chatService.createRoom(roomName);
     }
 
+    @Operation(summary = "전체 채팅방 조회 요청", description = "유저가 포함된 전체 채팅방 조회")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "방 전체 조회 성공",
+                    content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ChatRoom.class)))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "방 전체 조회 실패",
+                    content = @Content(mediaType = "application/json")
+            )
+    })
     @GetMapping
-    public List<ChatRoom> findAllRooms() {
+    public List<ChatRoom> findRoomByRoomName(@RequestParam("roomName") String roomName) {
         return chatService.findAllRooms();
+    }
+
+    @Operation(summary = "특정 채팅방 조회 요청", description = "유저가 입력한 이름을 가진 채팅방 조회")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "방 조회 성공",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChatRoom.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "방 조회 실패",
+                    content = @Content(mediaType = "application/json")
+            )
+    })
+    @PostMapping
+    public ChatRoom createChatRoom(@RequestParam("roomName") String roomName) {
+        return null;
     }
 
     // ws://localhost:8080/ws-stomp/pub/chat/message라는 발행 요청 + 데이터
