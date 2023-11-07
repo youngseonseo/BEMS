797fa41ac5aee68803f9f83695d4e37939ef5a36
hyeonji lee <hyunji3017@naver.com>
2023-11-07 11:06:59 +0900

    [fix] ChatRoom, ChatMessage 엔티티 수정

--- src/main/java/energypa/bems/chat/entity/ChatMessage.java
+++ src/main/java/energypa/bems/chat/entity/ChatMessage.java
@@ -1,36 +1,41 @@
 package energypa.bems.chat.entity;
 
+import energypa.bems.login.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-import java.time.LocalDateTime;
-
+@Schema(description = "채팅 메시지 info")
+@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@Table(name = "chat_message")
 public class ChatMessage {
 
+    @Schema(description = "채팅메시지 고유 번호", example = "1")
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "message_id")
     private Long messageId;
 
-    @Column
-    private String roomId;
+    @Schema(description = "채팅방 고유 번호", example = "1")
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private ChatRoom chatRoom;
 
-    @Column
+    @Schema(description = "채팅메시지 타입", example = "TALK")
+    @Enumerated(EnumType.STRING)
     private MessageType type;
 
-    @Column
-    private String sender;
+    @Schema(description = "채팅메시지 전송자", example = "1")
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member sender;
 
+    @Schema(description = "채팅메시지", example = "안녕하세요~")
     @Column
     private String content;
 
-    @Column
-    private LocalDateTime sentTime;
+    @Schema(description = "채팅메시지 전송 시각", example = "")
+    @Column(name = "sent_time")
+    private String sentTime; // 필드 타입 변경 필요
 }
--- src/main/java/energypa/bems/chat/entity/ChatRoom.java
+++ src/main/java/energypa/bems/chat/entity/ChatRoom.java
@@ -1,19 +1,22 @@
 package energypa.bems.chat.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
 
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
+@Schema(description = "채팅방 info")
+@Data
+@Entity(name = "chat_room")
 public class ChatRoom {
 
+    @Schema(description = "채팅방 고유 번호", example = "12345678-1234-5678-1234-567812345678")
     @Id
+    @Column(name = "room_id")
     private String roomId;
 
+    @Schema(description = "채팅방 유저 수", example = "4")
     @Column
-    private String roomName;
+    private int count;
 }
