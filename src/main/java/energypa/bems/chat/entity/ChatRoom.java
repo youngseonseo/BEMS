557e81fe6e7a266341b9408af5345556eff803ea
hyeonji lee <hyunji3017@naver.com>
2023-11-07 11:23:15 +0900

    [feat] ChatRoom 엔티티 수정

--- src/main/java/energypa/bems/chat/entity/ChatRoom.java
+++ src/main/java/energypa/bems/chat/entity/ChatRoom.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Schema(description = "채팅방 info")
 @Data
-@Entity(name = "chat_room")
+@Entity
+@Table(name = "chat_room")
 public class ChatRoom {
 
     @Schema(description = "채팅방 고유 번호", example = "12345678-1234-5678-1234-567812345678")
