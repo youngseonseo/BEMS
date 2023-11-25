53b4ee340b559964c6ccb9705e809c2a3fcda8aa
hyeonji lee <hyunji3017@naver.com>
2023-11-25 13:56:20 +0900

    [mod] 쿠폰 d-day 업데이트하는 스케줄러의 동작 시간 잘못 지정해서 변경

--- src/main/java/energypa/bems/coupon/scheduling/SchedulingDdayUpdate.java
+++ src/main/java/energypa/bems/coupon/scheduling/SchedulingDdayUpdate.java
@@ -14,7 +14,7 @@ public class SchedulingDdayUpdate {
     // 매일 자정(00:00:00)에 동작하는 스케줄러
     // 목적: 쿠폰의 d-day 업데이트
     // 대상: d-day 업데이트가 의미있는 쿠폰 - 사용 기한 내에 있으면서 아직 사용하지 않은 쿠폰
-    @Scheduled(cron = "0 42 13 * * ?")
+    @Scheduled(cron = "0 0 0 * * ?")
     public void updateDdayOfCoupon() {
 
         couponRepository.updateDdayOfCoupons();
