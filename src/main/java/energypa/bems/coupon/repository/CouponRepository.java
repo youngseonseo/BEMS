6a4cb1c15d0a20d703fad0984330a9ae79433ecb
hyeonji lee <hyunji3017@naver.com>
2023-11-25 13:53:33 +0900

    [add] 매일 자정, 쿠폰의 d-day를 업데이트하는 스케줄러 등록
    d-day 업데이트가 의미있는 쿠폰(유효기간 내에 있으면서 아직 사용하지 않은 쿠폰)에 대해서 매일 자정에 업데이트가 이뤄짐

--- src/main/java/energypa/bems/coupon/repository/CouponRepository.java
+++ src/main/java/energypa/bems/coupon/repository/CouponRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,4 +23,9 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
     @Modifying
     @Query("update Coupon cp set cp.usedYn = true where cp.couponId = :couponId")
     void updateUsedYnOfCoupon(@Param("couponId") long couponId);
+
+    @Transactional
+    @Modifying
+    @Query("update Coupon cp set cp.dDay = cp.dDay - 1 where cp.dDay >= 0 and cp.usedYn = false")
+    void updateDdayOfCoupons();
 }
---
+++ src/main/java/energypa/bems/coupon/scheduling/SchedulingDdayUpdate.java
@@ -0,0 +1,22 @@
+package energypa.bems.coupon.scheduling;
+
+import energypa.bems.coupon.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SchedulingDdayUpdate {
+
+    private final CouponRepository couponRepository;
+
+    // 매일 자정(00:00:00)에 동작하는 스케줄러
+    // 목적: 쿠폰의 d-day 업데이트
+    // 대상: d-day 업데이트가 의미있는 쿠폰 - 사용 기한 내에 있으면서 아직 사용하지 않은 쿠폰
+    @Scheduled(cron = "0 42 13 * * ?")
+    public void updateDdayOfCoupon() {
+
+        couponRepository.updateDdayOfCoupons();
+    }
+}
